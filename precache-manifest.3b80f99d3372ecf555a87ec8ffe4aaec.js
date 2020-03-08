69b9b86d68e4bffce91afcd9b0f10473ce991d38
Vitali <terrmit574@gmail.com>
2020-03-08 22:25:36 +0300

    Updates

---
+++ precache-manifest.3b80f99d3372ecf555a87ec8ffe4aaec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db09752d403da4c3d38b0309fc91cce",
+    "url": "/TMS-diplom/index.html"
+  },
+  {
+    "revision": "27c1845235fddccb696d",
+    "url": "/TMS-diplom/static/css/main.9cab8865.chunk.css"
+  },
+  {
+    "revision": "6f6ff87fe1e70932975d",
+    "url": "/TMS-diplom/static/js/2.0a09c0a8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/TMS-diplom/static/js/2.0a09c0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c1845235fddccb696d",
+    "url": "/TMS-diplom/static/js/main.150c4ff9.chunk.js"
+  },
+  {
+    "revision": "9a633e0c4104ff092952",
+    "url": "/TMS-diplom/static/js/runtime-main.92d05581.js"
+  }
+]);
